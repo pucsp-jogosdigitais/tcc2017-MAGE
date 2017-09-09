238b0690481dca97ccc41b238e5b1aec5c30939f
Daniel Gonçalves de Sene <30831387+DanielPitico@users.noreply.github.com>
2017-09-09 19:26:26 -0300

    Protejo Unity
    Está versão está atualizada com todas as movimentações funcionando

---
+++ ArchMAGEDown_v2.0/Assets/Scripts/Interações/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+	// Use this for initialization
+	Vector3 PosInicial;
+	public GameObject Personagem;
+	void Start()
+	{
+		PosInicial = this.transform.position;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		transform.position = Personagem.transform.position + PosInicial;
+	}
+}
---
+++ ArchMAGEDown_v2.0/Assets/Scripts/Interações/Spawnpoint.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawnpoint : MonoBehaviour
+{
+    public GameObject enemy;
+    public float spawnTime;
+    public float spawnRate;
+    public Transform spawnPoints;
+
+
+    void Start()
+    {
+        InvokeRepeating("Spawn", spawnTime, spawnRate);
+    }
+
+
+    void Spawn()
+    {
+        Instantiate(enemy, spawnPoints.position, spawnPoints.rotation);
+    }
+
+}
