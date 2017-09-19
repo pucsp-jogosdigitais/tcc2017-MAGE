3c50f508f2d5c224b211378753e3920436e2f7cd
Raffa <raffaelvidotto@gmail.com>
2017-09-19 14:54:04 -0300

    Versão 9000 removida, Mudanças nos menus

--- Assets/Scripts/Controles/MainMenu.cs
+++ Assets/Scripts/Controles/MainMenu.cs
@@ -5,39 +5,37 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    [SerializeField]
-    string firstlevel;
+	[SerializeField] string firstlevel;
 
-    [SerializeField]
-    string HubLevel;
-    // Use this for initialization
-    void Start()
-    {
+	[SerializeField] string HubLevel;
+	// Use this for initialization
+	void Start()
+	{
 
-    }
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
+	// Update is called once per frame
+	void Update()
+	{
 
-    }
+	}
 
-    public void NewGame()
-    {
-        SceneManager.LoadScene(firstlevel);
-    }
+	public void NewGame()
+	{
+		SceneManager.LoadScene(firstlevel);
+	}
 
 
-    public void Continue()
-    {
-        SceneManager.LoadScene(HubLevel);
-    }
+	public void Continue()
+	{
+		SceneManager.LoadScene(HubLevel);
+	}
 
 
-    public void Quit()
-    {
-        Application.Quit();
+	public void Quit()
+	{
+		Application.Quit();
 
-    }
+	}
 
 }
